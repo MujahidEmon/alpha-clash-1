5440b47edf53d454f4d8b9ce7b7fbe36b8cc3168
MujahidEmon <mujahidulislam.official@gmail.com>
2024-05-07 16:59:43 +0600

    part 1 comleted

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,6 +1,15 @@
+function continueGame(){
+    const alphabet = getRandomAlphabet();
+
+    document.getElementById('current-alphabet').innerText = alphabet;
+
+    setBgColorById(alphabet)
+}
 function playNow(){
     hideElementById('home-screen')
     showElementById('play-ground')
+    continueGame()
 }
+
 
 // document.getElementById('play-now-btn').addEventListener('click',playNow)
--- scripts/utility.js
+++ scripts/utility.js
@@ -6,3 +6,30 @@ function hideElementById(elementId){
 function showElementById(elementId){
     document.getElementById(elementId).classList.remove('hidden')
 }
+
+
+// add bg color by id
+
+function setBgColorById(elementId){
+    document.getElementById(elementId).classList.add('bg-orange-500')
+}
+
+function getRandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
+    const randomAlphabet = alphabetString.split('');
+    // console.log(randomAlphabet)
+
+    // get random index
+
+    const randomIndex = Math.random()*25;
+    const index = Math.round(randomIndex)
+
+    const alphabet = randomAlphabet[index];
+    return alphabet;
+}
+
+// displaying current alphabet
+
+// function displayCurrentAlphabet(){
+    
+// }
